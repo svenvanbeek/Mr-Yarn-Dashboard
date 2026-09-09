2b7ffd973284f658794ee4dc52cd35dc6bfcb1d3
svenvanbeek <scgvanbeek@gmail.com>
2026-09-09 16:17:38 +0200

    Delete route.ts

--- route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-const COOKIE_NAME = "mr_yarn_auth";
-
-export async function POST() {
-  const response = NextResponse.json({ ok: true });
-  response.cookies.delete(COOKIE_NAME);
-  return response;
-}
